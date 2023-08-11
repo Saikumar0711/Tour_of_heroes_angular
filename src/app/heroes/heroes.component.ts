dbcae5efaf6d0dfa539bbfd1055fe33cb67ca662
Saikumar0711 <111686035+Saikumar0711@users.noreply.github.com>
2023-08-11 18:50:34 +1000

    2 files modified

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -15,10 +15,13 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
+  selectedHero?:Hero;
 
   constructor() {}
 
-   onSelect(hero: Hero){}
+   onSelect(hero: Hero){
+     this.selectedHero=hero;
+   }
 
   
   ngOnInit() {}
